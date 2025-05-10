fbdc329a1e1a8696c2ed80e4726b687874c39a2a
Sha <sha.kongbrooks@gmail.com>
2025-05-11 05:19:14 +1000

    feat: add Artists component with SlideBackground and SlideGrid for visual presentation

---
+++ app/slides/Artists.tsx
@@ -0,0 +1,36 @@
+import { SlideBackground } from "~/components/SlideBackground";
+import { SlideGrid } from "~/components/SlideGrid";
+import { SlideGridImage } from "~/components/SlideGridImage";
+
+export function Artists() {
+  return (
+    <SlideBackground background="teal">
+      <SlideGrid size="grid-cols-4">
+        <div className="row-span-2">
+          <SlideGridImage
+            alt=""
+            src="/artem-bryzgalov-r2CAjGQ0gSI-unsplash.jpg"
+          />
+        </div>
+        <div className="row-span-2">
+          <SlideGridImage
+            alt=""
+            src="/artem-bryzgalov-r2CAjGQ0gSI-unsplash.jpg"
+          />
+        </div>
+        <div className="row-span-2">
+          <SlideGridImage
+            alt=""
+            src="/artem-bryzgalov-r2CAjGQ0gSI-unsplash.jpg"
+          />
+        </div>
+        <div className="row-span-2">
+          <SlideGridImage
+            alt=""
+            src="/artem-bryzgalov-r2CAjGQ0gSI-unsplash.jpg"
+          />
+        </div>
+      </SlideGrid>
+    </SlideBackground>
+  );
+}
